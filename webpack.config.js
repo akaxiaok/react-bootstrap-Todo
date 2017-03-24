54bf296c3e96b3cd655d7aaf4525255c420ed98b
akaxiaok <akaxiaok@gmail.com>
2017-03-24 23:18:04 +0800

    new

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Kimi on 2017/3/20.
+ */
+module.exports = {
+    entry: [
+        './src/js/index.js'
+    ],
+    output: {
+        filename: 'index.js',
+        publicPath: 'js/'
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+        ]
+    }
+};
