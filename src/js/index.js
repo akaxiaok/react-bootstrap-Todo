ac1d3496b91573c1409b0721e6797014ce197817
akaxiaok <akaxiaok@gmail.com>
2017-03-25 22:07:55 +0800

    bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -4,25 +4,55 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+let Todo = React.createClass({
+    render(){
+        let lists = [];
+        this.props.data.forEach(function (v, i) {
+            lists.push(<div key={i} className="input-group col-md-offset-2 col-md-3">
+                <CheckTodo/>
+                <Content content={v.content}/>
+                <DeleteTodo/>
+            </div>)
+        });
+        return (
+            <div className="    container-fluid">
+                {lists}
+            </div>
+        )
+    }
+});
+
 let Content = React.createClass({
     render(){
-        return<div className="well well-sm col-md-5">{this.props.content }</div> ;
+        return <input className="  form-control" defaultValue={this.props.content}/>;
     }
 });
-
-let DeleteButton = React.createClass({
+let CheckTodo = React.createClass({
     render(){
-        return <button className="btn btn-default col-md-1">x</button>;
+        return (
+            <span className="input-group-addon ">
+                <input type="checkbox" aria-label="..."/>
+            </span>
+        )
     }
 });
-
-let CompletBox = React.createClass({
+let DeleteTodo = React.createClass({
     render(){
-        return <input type="checkbox" className="col-md-1"/>
+        return (
+            <span className="input-group-btn">
+                <button className="btn btn-default" type="button">Delete</button>
+            </span>
+        );
     }
 });
-
+let data = [{
+    completed: false,
+    content: 'to do sth',
+}, {
+    completed: false,
+    content: 'to do sth2',
+}];
 ReactDOM.render(
-    <div className="row"><CompletBox/><Content content="todo"/><DeleteButton/></div>
+    <div className="row"><Todo data={data}/></div>
     , document.getElementById('example2')
 );
