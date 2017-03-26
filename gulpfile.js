6f47aad82d812cb184766d9d0f39c321ea95eb0e
akaxiaok <akaxiaok@gmail.com>
2017-03-26 16:56:26 +0800

    fonts

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp'),
     myth = require('gulp-myth'),
     webpackConf = require('./webpack.config'),
     webpack = require('gulp-webpack');
-gulp.task('default', ['style', 'webpack', 'html','index']);
+gulp.task('default', ['style', 'webpack', 'html', 'index', 'fonts']);
 
 gulp.task('style', function () {
     gulp.src('src/css/*.css').pipe(myth()).pipe(gulp.dest('build/css'));
     gulp.src('node_modules/bootstrap/dist/css/bootstrap.css').pipe(gulp.dest('build/css'));
+});
+gulp.task('fonts', function () {
+    gulp.src('node_modules/bootstrap/fonts/*').pipe(gulp.dest('build/fonts'));
 });
 gulp.task('html', function () {
     gulp.src('src/html/*.html').pipe(gulp.dest('build/html/'));
@@ -15,7 +18,7 @@ gulp.task('index', function () {
     gulp.src('src/*.html').pipe(gulp.dest('build/'));
 });
 gulp.task('webpack', function () {
-    gulp.src('src/index.js').pipe(webpack(webpackConf)).pipe(gulp.dest('build/js/')) ;
+    gulp.src('src/index.js').pipe(webpack(webpackConf)).pipe(gulp.dest('build/js/'));
 });
 var watcher = gulp.watch('src/index.html', ['index']);
 watcher.on('change', function (event) {
@@ -24,4 +27,6 @@ watcher.on('change', function (event) {
 });
 gulp.watch('src/html/*.html', ['html']);
 
-gulp.watch('src/js/*.js',['webpack']);
+gulp.watch('src/js/*.js', ['webpack']);
+
+gulp.watch('src/css/*.css', ['style']);
