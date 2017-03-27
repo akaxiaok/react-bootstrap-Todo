f10369608e3483be3536bdcd3eb4e57cba80a89e
akaxiaok <akaxiaok@gmail.com>
2017-03-27 21:14:38 +0800

    feat(todo): add addtodo component

---
+++ src/js/addtodo.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Kimi on 2017/3/27.
+ */
+import React from 'react';
+
+class AddTodo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleClick = e => {
+        e.preventDefault();
+        let input = e.target.querySelector('input');
+        if (!input.value) {
+            input.focus();
+            return;
+        }
+        this.props.addTodo(input.value);
+        input.value = '';
+        input.focus();
+
+    }
+
+    render() {
+        return (
+            <form className="row" onSubmit={this.handleClick}>
+                <div className="input-group">
+                    <input type="text" className="form-control" required placeholder="add new todo"/>
+                    <span className="input-group-btn">
+                       <button className="btn btn-default" type="submit">Add</button>
+                    </span>
+                </div>
+            </form>
+        )
+    }
+}
+export default AddTodo;
--- src/js/todo.js
+++ src/js/todo.js
@@ -5,6 +5,7 @@ import React from 'react';
 import data from './tododata';
 import TodoContent from './todocontent';
 import DeleteTodo from './deletetodo'
+import AddTodo from './addtodo';
 
 class Todo extends React.Component {
     constructor(props) {
@@ -17,24 +18,31 @@ class Todo extends React.Component {
     handleDelete = i => {
         this.state.data.splice(i, 1);
         this.setState({
-            data: data,
+            data: data
         });
     }
-
+    handleAddTodo = v => {
+        data.push({completed: false, content: v})
+        this.setState({
+            data: data
+        })
+    }
 
     render() {
         let lists = [];
         let handle = this.handleDelete;
         this.state.data.forEach(function (v, i) {
-            console.log(v, i);
-            lists.push(
-                <div key={i} className="" style={{fontSize: '24px'}}>
-                    <TodoContent id={i} content={v.content}/>
-                    <DeleteTodo id={i} onDelete={handle}/>
-                </div>)
+            if (!v.complete) {
+                lists.push(
+                    <div key={i} className="row" style={{fontSize: '24px'}}>
+                        <TodoContent id={i} content={v.content}/>
+                        <DeleteTodo id={i} onDelete={handle}/>
+                    </div>)
+            }
         });
         return (
             <div className="container">
+                <AddTodo addTodo={this.handleAddTodo}/>
                 {lists}
             </div>
         )
