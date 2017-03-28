a09213bcb11d1c9cde2f1b43e8735fdc3d0c0aa9
akaxiaok <akaxiaok@gmail.com>
2017-03-28 22:57:45 +0800

    feat(todo): add view change buttons component, add func to handle change view
    a button group to switch all/active/completed

--- src/js/todo.js
+++ src/js/todo.js
@@ -6,12 +6,13 @@ import data from './tododata';
 import TodoContent from './todocontent';
 import DeleteTodo from './deletetodo'
 import AddTodo from './addtodo';
-
+import {TodoView, VIEW_STATE} from './todoview'
 class Todo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             data: data,
+            currentState: VIEW_STATE.all
         }
     }
 
@@ -22,37 +23,59 @@ class Todo extends React.Component {
         });
     }
     handleAddTodo = v => {
-        data.push({completed: false, content: v})
-        this.setState({
-            data: data
-        })
+        data.push({completed: false, content: v});
+        this.setState({data: data});
+    }
+    switchView = s => {
+        switch (s) {
+            case VIEW_STATE.all:
+                this.setState({
+                    data: data,
+                    currentState: VIEW_STATE.all
+                });
+                break;
+            case VIEW_STATE.active:
+                this.setState({
+                    data: data.filter(x => x.completed === false),
+                    currentState: VIEW_STATE.active
+                });
+                break;
+            case VIEW_STATE.completed:
+                this.setState({
+                    data: data.filter(x => x.completed !== false),
+                    currentState: VIEW_STATE.completed
+                });
+                break;
+            default:
+                this.setState({data: data});
+                break;
+        }
     }
     toggleContent = (i) => {
-        data[i].completed = !data[i].completed;
+        data[data.indexOf(i)].completed = !i.completed;
         console.log(data);
-        this.setState({
-            data: data
-        })
+        this.switchView(this.state.currentState);
     }
 
     render() {
         let lists = [];
         this.state.data.forEach((v, i) => {
-            if (!v.completed) {
-                lists.push(
-                    <div key={i} className="row" style={{fontSize: '24px'}}>
-                        <TodoContent id={i} content={v.content} toggle={this.toggleContent}/>
-                        <DeleteTodo id={i} onDelete={this.handleDelete}/>
-                    </div>)
-            }
+            lists.push(
+                <div key={i} className="row" style={{fontSize: '24px'}}>
+                    <TodoContent id={i} data={v} toggle={this.toggleContent}/>
+                    <DeleteTodo id={i} onDelete={this.handleDelete}/>
+                </div>)
         });
         return (
             <div className="container">
                 <div className="page-header">
                     <h1>TodoList:</h1>
                 </div>
+                <TodoView onSwitch={this.switchView}/>
                 <AddTodo addTodo={this.handleAddTodo}/>
-                {lists}
+                <div className="content-list">
+                    {lists}
+                </div>
             </div>
         )
     }
