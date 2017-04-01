b9f9dac06a396996aa91bf4127086206f070c463
akaxiaok <akaxiaok@gmail.com>
2017-04-01 21:16:54 +0800

    feat(webpack): use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 /**
  * Created by Kimi on 2017/3/20.
  */
+var webpack = require('webpack');
+var path = require('path');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 module.exports = {
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        contentBase: './src',
+        port: 8080
+    },
     entry: [
         './src/js/index.js'
     ],
@@ -12,7 +22,18 @@ module.exports = {
     },
     module: {
         loaders: [
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
             {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream"},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader"},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml"},
+            {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"},
+
         ]
-    }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new OpenBrowserPlugin({url: 'http://localhost:8080'})
+    ]
 };
