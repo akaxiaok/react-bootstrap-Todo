f25226e89c493035e52e402d86d80132a5eb21c3
akaxiaok <akaxiaok@gmail.com>
2017-04-03 18:08:23 +0800

    refactor(redux.test): add redux test

---
+++ src/test/redux/redux.test.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Kimi on 2017/4/3.
+ */
+import  {addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters} from '../../js/actions';
+import  store from '../../js/store';
+
+// Log the initial state
+console.log(store.getState());
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+let unsubscribe = store.subscribe(() =>
+    console.log(store.getState())
+);
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'));
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about store'));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+// Stop listening to state updates
+unsubscribe();
