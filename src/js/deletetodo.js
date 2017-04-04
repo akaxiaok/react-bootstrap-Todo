9e4ee8f9a9d7add7a19bcec1ab0884d05449e088
akaxiaok <akaxiaok@gmail.com>
2017-04-04 19:04:46 +0800

    feat(deletetodo): use redux

--- src/js/deletetodo.js
+++ src/js/deletetodo.js
@@ -2,7 +2,8 @@
  * Created by Kimi on 2017/3/27.
  */
 import React from 'react';
-
+import {connect} from 'react-redux';
+import {deleteTodo} from './actions';
 class DeleteTodo extends React.Component {
     constructor(props) {
         super(props);
@@ -11,14 +12,21 @@ class DeleteTodo extends React.Component {
     render() {
         return (
             <a type="button" className="" href="#" style={{marginLeft: '.1em', verticalAlign: '-.1em'}}
-               onClick={this.handleClick}>
+               onClick={this.props.handleClick.bind(this, this.props.todo)}>
                 <span className="glyphicon glyphicon-remove" aria-hidden="true"/>
             </a>
         );
     }
 
-    handleClick = () => {
-        this.props.onDelete(this.props.data);
-    }
 }
+function mapDispatchToProps(dispatch) {
+    return {
+        handleClick: (todo) => {
+            dispatch(deleteTodo(todo))
+        }
+    };
+}
+
+DeleteTodo = connect(null, mapDispatchToProps)(DeleteTodo)
+
 export default DeleteTodo;
