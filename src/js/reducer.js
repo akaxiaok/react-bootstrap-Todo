badc8ff17f53ff6fd338197340bb72f4f6b14b1c
akaxiaok <akaxiaok@gmail.com>
2017-04-04 19:06:12 +0800

    feat(reducer): add new

--- src/js/reducer.js
+++ src/js/reducer.js
@@ -2,10 +2,14 @@
  * Created by Kimi on 2017/4/3.
  */
 import {VisibilityFilters} from './actions'
-import  {ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER} from './actions';
+import  {START_EDIT, END_EDIT, ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, DELETE_TODO} from './actions';
 const initialState = {
     visibilityFilter: VisibilityFilters.SHOW_ALL,
-    todos: []
+    todos: [{content: '1', completed: false, edit: false}, {content: '2', completed: false, edit: false}, {
+        content: '3',
+        completed: true,
+        edit: false
+    }]
 };
 
 export function todoApp(state = initialState, action) {
@@ -20,19 +24,54 @@ export function todoApp(state = initialState, action) {
                     ...state.todos,
                     {
                         content: action.text,
-                        completed: false
+                        completed: false,
+                        edit: false
                     }
-                ]
+                ],
+                visibilityFilter: state.visibilityFilter === VisibilityFilters.SHOW_COMPLETED ? VisibilityFilters.SHOW_ACTIVE : state.visibilityFilter
             });
         case TOGGLE_TODO:
             return Object.assign({}, state, {
                 todos: state.todos.map((todo, index) => {
-                    if (index === action.index) {
+                    if (todo === action.todo) {
                         return Object.assign({}, todo, {
                             completed: !todo.completed
                         })
                     }
                     return todo
+                })
+            });
+        case DELETE_TODO:
+            console.log(action.todo);
+            state.todos.splice(state.todos.indexOf(action.todo), 1);
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo) => todo)
+            });
+        case START_EDIT:
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo) => {
+                    if (todo === action.todo) {
+                        return Object.assign({}, todo, {
+                            edit: true
+                        })
+                    }
+                    else {
+                        return todo;
+                    }
+                })
+            });
+        case END_EDIT:
+            return Object.assign({}, state, {
+                todos: state.todos.map((todo) => {
+                    if (todo === action.todo) {
+                        return Object.assign({}, todo, {
+                            content: action.value,
+                            edit: false
+                        })
+                    }
+                    else {
+                        return todo;
+                    }
                 })
             });
         default:
