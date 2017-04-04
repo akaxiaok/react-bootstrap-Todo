ec3f019790bc3737701dd0eb5caf4078ab654546
akaxiaok <akaxiaok@gmail.com>
2017-04-04 19:05:39 +0800

    feat(actions): add new actions

--- src/js/actions.js
+++ src/js/actions.js
@@ -3,7 +3,9 @@
  */
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
-
+export const DELETE_TODO = 'DELETE_TODO';
+export const START_EDIT = 'START_EDIT';
+export const END_EDIT = 'END_EDIT';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER ';
 export const VisibilityFilters = {
     SHOW_ALL: 'SHOW_ALL',
@@ -11,16 +13,36 @@ export const VisibilityFilters = {
     SHOW_ACTIVE: 'SHOW_ACTIVE'
 };
 
+export function startEdit(todo) {
+    return {
+        type: START_EDIT,
+        todo
+    }
+}
+export function endEdit(todo, value) {
+    return {
+        type: END_EDIT,
+        todo,
+        value
+    }
+}
+
 export function addTodo(text) {
     return {
         type: ADD_TODO, text
     }
 }
-export function toggleTodo(index) {
+export function toggleTodo(todo) {
     return {
-        type: TOGGLE_TODO, index
+        type: TOGGLE_TODO, todo
     }
 }
+export function deleteTodo(todo) {
+    return {
+        type: DELETE_TODO, todo
+    }
+}
+
 export function setVisibilityFilter(filter) {
     return {type: SET_VISIBILITY_FILTER, filter}
 }
