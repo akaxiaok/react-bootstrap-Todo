c5c51ec1960e1144823e06d030d2afc3b8d88aa1
akaxiaok <akaxiaok@gmail.com>
2017-04-04 19:05:13 +0800

    feat(index): use redux

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,13 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Todo from './todo'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {todoApp} from './reducer';
+let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 ReactDOM.render(
-    <Todo />
+    <Provider store={store}>
+        <Todo />
+    </Provider>
     , document.getElementById('todo')
 );
