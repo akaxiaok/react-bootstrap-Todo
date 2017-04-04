ede8101c7d7a17a922555a10370219518a509c27
akaxiaok <akaxiaok@gmail.com>
2017-04-04 19:05:03 +0800

    feat(edittodo): use redux

--- src/js/edittodo.js
+++ src/js/edittodo.js
@@ -2,7 +2,8 @@
  * Created by Kimi on 2017/3/29.
  */
 import React from 'react';
-
+import {startEdit} from './actions';
+import {connect} from 'react-redux';
 class EditTodo extends React.Component {
     constructor(props) {
         super(props);
@@ -11,14 +12,21 @@ class EditTodo extends React.Component {
     render() {
         return (
             <a type="button" className="" href="#" style={{marginLeft: '.1em', verticalAlign: '-.1em'}}
-               onClick={this.handleClick}>
+               onClick={this.props.handleClick.bind(this, this.props.todo)}>
                 <span className="glyphicon glyphicon-edit" aria-hidden="true"/>
             </a>
         );
     }
-
-    handleClick = () => {
-        this.props.startEdit(this.props.data);
-    }
 }
+
+function mapDispatchToProps(dispatch) {
+    return {
+        handleClick: (todo) => {
+            dispatch(startEdit(todo));
+        }
+    };
+}
+
+
+EditTodo = connect(null, mapDispatchToProps)(EditTodo);
 export default EditTodo;
