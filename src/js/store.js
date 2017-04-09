b9a8160328c9748e8581049f2340273be30e2261
akaxiaok <akaxiaok@gmail.com>
2017-04-09 12:53:52 +0800

    refactor(store): change import

--- src/js/store.js
+++ src/js/store.js
@@ -2,6 +2,6 @@
  * Created by Kimi on 2017/4/3.
  */
 import {createStore}  from 'redux';
-import {todoApp}  from './reducer';
-let store = createStore(todoApp);
+import todoApp  from './reducer';
+let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
