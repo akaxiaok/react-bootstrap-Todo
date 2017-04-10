28d044aafae61b5c640ac48012233561d95b0f60
akaxiaok <akaxiaok@gmail.com>
2017-04-10 23:38:31 +0800

    refactor(*): test redux async

--- src/test/redux/reduxasync.test.js
+++ src/test/redux/reduxasync.test.js
@@ -2,12 +2,13 @@
  * Created by Kimi on 2017/4/10.
  */
 import promiseMiddleware from 'redux-promise';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, dispatch} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rq from 'request-promise-native';
+import fetch from 'isomorphic-fetch';
 const FETCH_THING = 'FETCH_THING';
 let rootReducer = function (state = {}, action) {
-    switch (action) {
+    switch (action.type) {
         case FETCH_THING:
             return action.result;
         default:
@@ -17,30 +18,30 @@ let rootReducer = function (state = {}, action) {
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(promiseMiddleware)
+    applyMiddleware(thunkMiddleware)
 );
 
 
-let getAction = async (language) => {
-    let options = {
-        uri: `https://api.github.com`,
-        headers: {
-            'User-Agent': 'Request-Promise'
-        }
-    };
-    const result = await rq(options);
-    console.log(result);
-    return {
-        type: FETCH_THING,
-        result: result
-    };
+let getThunkAction = (language) => {
+    return (dispatch) => {
+        return fetch(`https://api.github.com`).then(response => response.json()).then(
+            json => {
+                console.log(json);
+                dispatch({
+                    type: FETCH_THING,
+                    result: json
+                })
+            }
+        )
+    }
 };
-
+store.getState();
 
 let unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
 });
-store.dispatch(getAction('javascript'));
-console.log(store.getState());
-store.dispatch(getAction('java'));
-console.log(store.getState());
+store.dispatch(getThunkAction('java'));
+
+
+unsubscribe();
 
