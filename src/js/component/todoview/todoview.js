128248242943e099621cd1686c23480e4b60f763
akaxiaok <akaxiaok@gmail.com>
2017-08-15 21:14:26 +0800

    refactor(*): divide to container & component

--- src/js/component/todoview/todoview.js
+++ src/js/component/todoview/todoview.js
@@ -1,36 +1,33 @@
 import React from 'react';
-import Unredo from '../unredo'
-import { VisibilityFilters } from '../../actions';
+import {Unredo} from '../index'
+import {VisibilityFilters} from '../../actions';
 
 class TodoView extends React.Component {
-  getStyle = (x) => {
-    if (x === this.props.currentState) {
-      return "btn btn-primary";
+    getStyle = (x) => {
+        if (x === this.props.currentState) {
+            return "btn btn-primary";
+        }
+        else {
+            return "btn btn-default";
+        }
     }
-    else {
-      return "btn btn-default";
-    }
-  }
 
-  render() {
-    return (
-      <div >
-        <div className="btn-group view-buttons" role="group" aria-label="..." >
-          <button type="button" className={this.getStyle(VisibilityFilters.SHOW_ALL)}
-                  onClick={this.props.handleClick.bind(this, VisibilityFilters.SHOW_ALL)} >
-            All
-          </button >
-          <button type="button" className={this.getStyle(VisibilityFilters.SHOW_ACTIVE)}
-                  onClick={this.props.handleClick.bind(this, VisibilityFilters.SHOW_ACTIVE)} >Active
-          </button >
-          <button type="button" className={this.getStyle(VisibilityFilters.SHOW_COMPLETED)}
-                  onClick={this.props.handleClick.bind(this, VisibilityFilters.SHOW_COMPLETED)} >Completed
-          </button >
-        </div >
-        <Unredo {...this.props.unredos}/>
-      </div >
-    )
-  }
+    render() {
+        return (
+            <div className="btn-group view-buttons" role="group" aria-label="...">
+                <button type="button" className={this.getStyle(VisibilityFilters.SHOW_ALL)}
+                        onClick={this.props.handleClick.bind(this, VisibilityFilters.SHOW_ALL)}>
+                    All
+                </button>
+                <button type="button" className={this.getStyle(VisibilityFilters.SHOW_ACTIVE)}
+                        onClick={this.props.handleClick.bind(this, VisibilityFilters.SHOW_ACTIVE)}>Active
+                </button>
+                <button type="button" className={this.getStyle(VisibilityFilters.SHOW_COMPLETED)}
+                        onClick={this.props.handleClick.bind(this, VisibilityFilters.SHOW_COMPLETED)}>Completed
+                </button>
+            </div>
+        )
+    }
 }
 
 
--- src/js/container/todoview.js
+++ src/js/container/todoview.js
@@ -2,38 +2,33 @@
  * Created by Kimi on 2017/3/28.
  */
 import React from 'react';
-import { ActionCreators } from 'redux-undo'
-import { connect } from 'react-redux';
-import { setVisibilityFilter } from '../actions';
-import { TodoView } from '../component';
+import {ActionCreators} from 'redux-undo'
+import {connect} from 'react-redux';
+import {setVisibilityFilter} from '../actions';
+import {TodoView} from '../component';
 
-
-class TodoViewContainer extends React.Component {
-  render() {
-    const { currentState, handleClick, canUndo, canRedo, undo, redo } = this.props;
-    const unredos = { canUndo: canUndo, canRedo: canRedo, undo: undo, redo: redo };
+const TodoViewContainer = (props) => {
     return (
-      <TodoView currentState={currentState} handleClick={handleClick} unredos={unredos} />
+        <TodoView {...props}/>
     )
-  }
 }
 
 function mapStateToPorps(state) {
-  return {
-    currentState: state.visibilityFilter,
-    canUndo: state.todos.past.length > 0,
-    canRedo: state.todos.future.length > 0
-  }
+    return {
+        currentState: state.visibilityFilter,
+        canUndo: state.todos.past.length > 0,
+        canRedo: state.todos.future.length > 0
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    handleClick: (filter) => {
-      dispatch(setVisibilityFilter(filter))
-    },
-    undo: () => dispatch(ActionCreators.undo()),
-    redo: () => dispatch(ActionCreators.redo())
-  };
+    return {
+        handleClick: (filter) => {
+            dispatch(setVisibilityFilter(filter))
+        },
+        undo: () => dispatch(ActionCreators.undo()),
+        redo: () => dispatch(ActionCreators.redo())
+    };
 }
 
 export default connect(mapStateToPorps, mapDispatchToProps)(TodoViewContainer);
