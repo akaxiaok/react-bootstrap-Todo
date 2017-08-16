18e1f1d59d569bbe21152bfda2ed78008ed64a2c
akaxiaok <akaxiaok@gmail.com>
2017-08-16 21:31:15 +0800

    refactor(addtodo): use Controlled Components

--- src/js/component/addtodo/addtodo.js
+++ src/js/component/addtodo/addtodo.js
@@ -7,24 +7,27 @@ import {VisibilityFilters} from '../../actions';
 class AddTodo extends React.Component {
     handleClick = e => {
         e.preventDefault();
-        let input = e.target.querySelector('input');
-        if (!input.value) {
-            input.focus();
-            return;
-        }
         if (this.props.visibilityFilter === VisibilityFilters.SHOW_COMPLETED) {
             this.props.setVisibilityFilter(VisibilityFilters.SHOW_ACTIVE);
         }
-        this.props.addTodo(input.value);
-        input.value = '';
-        input.focus();
+        this.props.addTodo(this.state.value);
+        this.setState({value: ''});
+    }
+    handleChange = e => {
+        this.setState({value: e.target.value})
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
     }
 
     render() {
         return (
             <form onSubmit={this.handleClick}>
                 <div className="input-group col-sm-5">
-                    <input type="text" className="form-control" required placeholder="add new todo"/>
+                    <input type="text" className="form-control" onChange={this.handleChange} required
+                           placeholder="add new todo" value={this.state.value}/>
                     <span className="input-group-btn">
                        <button className="btn btn-default" type="submit">Add</button>
                     </span>
